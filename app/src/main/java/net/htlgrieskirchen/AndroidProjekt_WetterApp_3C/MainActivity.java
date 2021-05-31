a7a090ecc34bf29756f3b767d904a8908172e778
erikson835 <Neulinger.Erik@sus.htl-grieskirchen.at>
2021-05-31 19:44:35 +0200

    initial commit

---
+++ app/src/main/java/net/htlgrieskirchen/AndroidProjekt_WetterApp_3C/MainActivity.java
@@ -0,0 +1,14 @@
+package net.htlgrieskirchen.AndroidProjekt_WetterApp_3C;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
