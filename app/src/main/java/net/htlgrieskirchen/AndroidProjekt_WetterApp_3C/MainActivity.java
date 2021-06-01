73ab0fb3747a891c0e5b5d1b4599a45692fe0309
Motsch04 <moritzrazenberger123@gmail.com>
2021-06-01 17:06:03 +0200

    Standort hinzufuegen commit

--- app/src/main/java/net/htlgrieskirchen/AndroidProjekt_WetterApp_3C/MainActivity.java
+++ app/src/main/java/net/htlgrieskirchen/AndroidProjekt_WetterApp_3C/MainActivity.java
@@ -1,16 +1,72 @@
 package net.htlgrieskirchen.AndroidProjekt_WetterApp_3C;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button button;
+    ListView listView;
+    private ArrayList<String> list = new ArrayList<>();
+    private ArrayAdapter<String> adapter;
+    String adresse;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("Test");
-        System.out.println("Test3");
+
+        listView = findViewById(R.id.listView);
+        bindAdapterToListView(listView);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(this);
+    }
+
+    private void bindAdapterToListView(ListView lv) {
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
+        lv.setAdapter(adapter);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
+        alertDialog.setMessage("Geben sie ihren gewünschten Standort ein:");
+
+        final EditText input = new EditText(MainActivity.this);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT);
+        input.setLayoutParams(lp);
+        alertDialog.setView(input);
+
+        alertDialog.setPositiveButton("OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        adresse = input.getText().toString();
+                        list.add(adresse);
+                    }
+                });
+
+        alertDialog.setNegativeButton("CANCEL",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+        alertDialog.show();
     }
 }
